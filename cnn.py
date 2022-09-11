bcca61ea6fcf51c0794e34810d52fd8e5910569f
Sarvesh Agrawal <36103211+Sarvesh1523@users.noreply.github.com>
2022-09-11 22:04:51 +0530

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -1,1 +1,80 @@
+import tensorflow
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
 
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, BatchNormalization, Activation, MaxPooling2D
+
+img_size, batch_size, epochs = 224, 16, 20
+
+train_datagenerator = ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
+validation_datagenerator = ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
+
+train_generator = train_datagenerator.flow_from_directory("path", target_size=(img_size, img_size), color_mode="grayscale", batch_size=batch_size, class_mode="binary", shuffle=True)
+validation_generator = validation_datagenerator.flow_from_directory("path", target_size=(img_size, img_size), color_mode="grayscale", batch_size=batch_size, class_mode="binary", shuffle=True)
+
+METRICS = [
+      TruePositives(name='tp'),
+      FalsePositives(name='fp'),
+      TrueNegatives(name='tn'),
+      FalseNegatives(name='fn'), 
+      BinaryAccuracy(name='accuracy'),
+      Precision(name='precision'),
+      Recall(name='recall'),
+      AUC(name='auc'),
+]
+
+model = Sequential()
+
+# 1. Conv
+model.add(Conv2D(64, (3, 3), padding='same', input_shape=(img_size, img_size, 1)))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+
+# 2. Conv Layer
+model.add(Conv2D(128, (5, 5), padding='same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+# 3. Conv Layer
+model.add(Conv2D(512, (3,3), padding='same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+#4. Conv Layer
+model.add(Conv2D(512, (3,3), padding='same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(256))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Dropout(0.25))
+
+model.add(Dense(512))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Dropout(0.25))
+
+model.add(Dense(1, activation='sigmoid'))
+model.compile(optimizer="adam", loss='binary_crossentropy', metrics=METRICS)
+model.summary()
+
+spe = train_generator.n // train_generator.batch_size
+val_steps = validation_generator.n // validation_generator.batch_size
+
+ckpt = ModelCheckpoint("/gdrive/MyDrive/Drowsiness/model/orignal_model.h5", monitor='val_accuracy', save_weights_only=True, mode='max', verbose=1)
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, min_lr=0.00001, model='auto')
+callbacks=[PlotLossesCallback(), ckpt, reduce_lr]
+
+history_base = model.fit(x=train_generator, steps_per_epoch=spe, epochs=epochs, validation_data=validation_generator, validation_steps=val_steps, callbacks=callbacks)
